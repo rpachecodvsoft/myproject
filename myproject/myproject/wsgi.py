f01f86206a51f0cc3fddc39b67118cccd9d68ff8
rpachecodvsoft <rpacheco.dvsoft@gmail.com>
2025-06-30 12:31:20 -0400

    Hello world v8

--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.myproject.settings')
 
 application = get_wsgi_application()
