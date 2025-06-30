ba563e66e3744159ec2bade5ed08576bf860ee23
rpachecodvsoft <rpacheco.dvsoft@gmail.com>
2025-06-30 15:54:35 -0400

    Hello world v13.6

--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
 
-print("DJANGO_SETTINGS_MODULE =", os.environ.get('DJANGO_SETTINGS_MODULE'))
+print(">>> DJANGO_SETTINGS_MODULE (wsgi) =", os.environ.get('DJANGO_SETTINGS_MODULE'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.myproject.settings')
 
